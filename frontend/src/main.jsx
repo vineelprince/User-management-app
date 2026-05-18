fd2547243a048b429beb55a097b9a86ff7ba7988
vineel <vineel183sainik@gmail.com>
2026-05-18 19:55:49 +0530

    fixed frontend routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { HashRouter } from "react-router-dom";
+
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
